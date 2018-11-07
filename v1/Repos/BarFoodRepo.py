634e225d7a47fa15c8e99d7bfad8d310d22b131f
Zer0Name <aap220@scarletmail.rutgers.edu>
2018-11-07 18:31:29 -0500

    add insert, delete, update, not tested

--- v1/Repos/BarFoodRepo.py
+++ v1/Repos/BarFoodRepo.py
@@ -11,3 +11,19 @@ class BarFoodRepo(SQL.SQL_table):
 		sql = "Select * from BarFood"
 		items = self.query(sql,BarFood)
 		return items
+
+	def insertBarFood(self,BarFood):
+		sql = "INSERT INTO BarFood VALUES (%s)"
+		vals = (BarFood.getName(),)
+		return self.insert(sql,vals)
+
+
+	def updateBarFood(self,BarFood,oldName):
+		sql = "UPDATE BarFood SET name = %s WHERE name = %s "
+		vals = (BarFood.getName(),oldName)
+		return self.update(sql,vals)
+
+	def deleteBarFood(self,BarFood):
+		sql = "DELETE FROM BarFood WHERE name = %s "
+		vals = (BarFood.getName(),)
+		return self.delete(sql,vals)
