2b9c7bd60cb07dde3546fe6ef37764662ebec4e0
jennykoshka <jns131@scarletmail.rutgers.edu>
2018-11-08 00:41:31 -0500

    sells beer and food modification done

--- v1/Repos/SellsFoodRepo.py
+++ v1/Repos/SellsFoodRepo.py
@@ -11,3 +11,19 @@ class SellsFoodRepo(SQL.SQL_table):
 		sql = "Select * from SellsFood"
 		items = self.query(sql,SellsFood)
 		return items
+	
+	def insertSellsFood(self,sellsFood):
+		sql = "INSERT INTO SellsFood (foodname, barname, price) VALUES (%s, %s, %s)"
+		vals = (sellsFood.getFood(),sellsFood.getBar(), sellsFood.getPrice())
+		return self.insert(sql,vals)
+
+
+	def updateSellsFood(self,sellsFood,oldFood, oldBar):
+		sql = "UPDATE SellsFood SET foodname = %s, barname = %s, price = %s WHERE foodname = %s, barname = %s "
+		vals = (sellsFood.getFood(),sellsFood.getBar(), sellsFood.getPrice(), oldFood, oldBar)
+		return self.update(sql,vals)
+
+	def deleteSellsFood(self,sellsFood):
+		sql = "DELETE FROM SellsFood WHERE foodname = %s, barname = %s "
+		vals = (sellsFood.getFood(),sellsFood.getBar())
+		return self.delete(sql,vals)
---
+++ v1/Services/SellsBeerService.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, Blueprint, request, json, make_response
+import v1.Repos.SellsBeerRepo as SellsBeerRepo
+
+'''
+checks to make:
+no duplicate entries
+beer exists
+bar exists
+pattern #3
+'''
+def insertSellsBeer(sellsBeer):
+	sellsBeerRepo = SellsBeerRepo.SellsBeerRepo()
+	return sellsBeerRepo.insertSellsBeer(sellsBeer)
+
+'''
+checks to make:
+no duplicate entries
+beer exists
+bar exists
+pattern #3
+'''
+def updateSellsBeer(sellsBeer,oldBeer, oldBar):
+	sellsBeerRepo = SellsBeerRepo.SellsBeerRepo()
+	return sellsBeerRepo.updateSellsBeer(sellsBeer, oldBeer, oldBar)
+
+''' no check cascade should be handled by table '''
+def deleteSellsBeer(sellsBeer):
+	sellsBeerRepo = SellsBeerRepo.SellsBeerRepo()
+	return sellsBeerRepo.deleteSellsBeer(sellsBeer)
