3708bf05798c0c5f0266be0cf521eba1181c653d
Zer0Name <aap220@scarletmail.rutgers.edu>
2018-11-10 19:09:44 -0500

    add path

--- v1/Controllers/DailyController.py
+++ v1/Controllers/DailyController.py
@@ -5,8 +5,16 @@ import v1.Util.Variable as variable
 dailyController = Blueprint('dailyController', __name__)
 
 import v1.Services.InventoryService as inventoryService
+import v1.Services.ShiftsService as shiftsService
 
 
 @dailyController.route('/update/inventory', methods=['POST'])
 def updateInventoryForToday():
     return inventoryService.insertInventoryForToday()
+
+
+@dailyController.route('/update/shifts', methods=['POST'])
+def updateShiftsForToday():
+    return shiftsService.getLastDate()
+
+
