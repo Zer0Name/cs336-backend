197c6ad54a61beafe8402779293f2a877b32bcb6
jennykoshka <jns131@scarletmail.rutgers.edu>
2018-11-12 20:39:12 -0500

    added checks for insert Bills

--- v1/Repos/BillsRepo.py
+++ v1/Repos/BillsRepo.py
@@ -1,6 +1,8 @@
 import v1.Repos.SQL as SQL
 
 from v1.Entity.Bills import Bills
+from v1.Entity.Transactions import Transactions
+from v1.DTO.TrueFalseDTO import TrueFalseDTO
 
 class BillsRepo(SQL.SQL_table):
 	
@@ -26,3 +28,64 @@ class BillsRepo(SQL.SQL_table):
 		sql = "DELETE FROM Bills WHERE bill_id = %s "
 		vals = (bills.getBillId())
 		return self.delete(sql,vals)
+
+	#checks for insert and update
+	def bar_exists(self, bar):
+		sql = "SELECT NOT EXISTS(SELECT * FROM Bar WHERE name = \""+str(bar)+"\") AS value"
+		items = self.query(sql,TrueFalseDTO)
+		if items[0].value == 1:
+			return True
+		return False
+
+	def drinker_exists(self, drinker):
+		sql = "SELECT NOT EXISTS(SELECT * FROM Drinker WHERE name = \""+str(drinker)+"\") AS value"
+		items = self.query(sql,TrueFalseDTO)
+		if items[0].value == 1:
+			return True
+		return False
+	
+	def bartender_exists(self, bartender):
+		sql = "SELECT NOT EXISTS(SELECT * FROM Bartender WHERE name = \""+str(bartender)+"\") AS value"
+		items = self.query(sql,TrueFalseDTO)
+		if items[0].value == 1:
+			return True
+		return False
+
+
+	def duplicate_entry(self, bill_id):
+		sql = "SELECT EXISTS(SELECT * FROM Bills WHERE bill_id = \""+str(bill_id)+"\") AS value"
+		items = self.query(sql,TrueFalseDTO)
+		if items[0].value == 1:
+			return True
+		return False
+	
+	def time_during_shift(self, time, bartender, bar, date):
+		sql = "SELECT EXISTS(SELECT * FROM Shifts WHERE bartender = \""+str(bartender)+"\" \
+				AND bar = \""+str(bar)+"\" AND date = \""+str(date)+"\" \
+				AND \""+str(time)+"\" >= start AND \""+str(time)+"\" <= end) AS value)"
+		items = self.query(sql,TrueFalseDTO)
+		if items[0].value == 1:
+			return True
+		return False
+	
+	#covers pattern 1
+	def time_during_operating_hours(self, time, bar, date):
+		sql = "SELECT EXISTS(SELECT * FROM Operates WHERE  \
+				AND bar = \""+str(bar)+"\" AND date = \""+str(date)+"\" \
+				AND \""+str(time)+"\" >= start AND \""+str(time)+"\" <= end) AS value)"
+		items = self.query(sql,TrueFalseDTO)
+		if items[0].value == 1:
+			return True
+		return False
+	
+	def check_items_price(self, bill_id, items_price):
+		sql = "SELECT * FROM Transactions WHERE bill_id = \""+str(bill_id)+"\""
+		items = self.query(sql,Transactions)
+		count = 0
+		for t in items:
+			count = count + t.getPrice()
+		if count == items_price:
+			return True
+		return False
+	
+	#check tax and total in service since no queries needed, same for if day corresponds to date
--- v1/Services/BillsService.py
+++ v1/Services/BillsService.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, Blueprint, request, json, make_response
 import v1.Repos.BillsRepo as BillsRepo
+from v1.Exceptions.InvalidInfo import InvalidInfo
+from datetime import datetime, timedelta
+import calendar
 
 def getAllBills():
-    billsRepo = BillsRepo.BillsRepo() 
-    results = billsRepo.getAllBills()
-    return  jsonify([e.toJson() for e in results])
+	billsRepo = BillsRepo.BillsRepo() 
+	results = billsRepo.getAllBills()
+	return  jsonify([e.toJson() for e in results])
 
 '''
 checks to make:
@@ -23,6 +26,28 @@ time is during bar's hours and during bartender's shift
 '''
 def insertBills(bills):
 	billsRepo = BillsRepo.BillsRepo()
+	if not billsRepo.bar_exists(bills.getBar()):
+		raise InvalidInfo("bar does not exist") 
+	if not billsRepo.bartender_exists(bills.getBartender()):
+		raise InvalidInfo("bartender does not exist") 
+	if not billsRepo.drinker_exists(bills.getDrinker()):
+		raise InvalidInfo("drinker does not exist") 
+	datetime_object = datetime.strptime(bills.getDate(), "%Y-%m-%d")
+	if not calendar.day_name[datetime_object.weekday()] == bills.getDay():
+		raise InvalidInfo("Day does not match date")
+	if billsRepo.duplicate_entry(bills.getBillId()):
+		raise InvalidInfo("duplicate entry") 
+	if not billsRepo.time_during_shift(bills.getTime(), bills.getBartender(), bills.getBar(), bills.getDate()):
+		raise InvalidInfo("Bartender does not have shift at the bar and/or on that date and/or during that time") 
+	if not billsRepo.time_during_operating_hours(bills.getTime(), bills.getBar(), bills.getDate()):
+		raise InvalidInfo("Bar is not open during that time")
+	if not billsRepo.check_items_price(bills.getBillId(), bills.getItemsPrice()):
+		raise InvalidInfo("Price of all the items doesn't match the total of the corresponding transactions") 
+	if not bills.getTaxPrice() == bills.getItemsPrice()*0.07:
+		raise InvalidInfo("Tax is not 7 percent of the items price")
+	if not bills.getTaxPrice() + bills.getTip() + bills.getItemsPrice() == bills.getTotalPrice():
+		raise InvalidInfo("Incorrect total price")
+	
 	return billsRepo.insertBills(bills)
 
 '''
