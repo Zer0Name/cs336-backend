fc25a46a1da67157c29a8deeff8dc6690eb00035
Zer0Name <aap220@scarletmail.rutgers.edu>
2018-11-16 00:14:32 +0200

    fix insert and update call

--- v1/Repos/BillsRepo.py
+++ v1/Repos/BillsRepo.py
@@ -21,12 +21,12 @@ class BillsRepo(SQL.SQL_table):
 
 	def insertBills(self,bills):
 		sql = "INSERT INTO Bills (bill_id, bar, drinker, date, items_price, tax_price, tip, total_price, time, bartender, day) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-		vals = (bills.getBillId(), bills.getBar(), bills.getDrinker(), bills.getDate(),bills.getItemsPrice(), bills.getTax(), bills.getTip(), bills.getTotalPrice(), bills.getTime(), bills.getBartender(), bills.getDay())
+		vals = (bills.getBillId(), bills.getBar(), bills.getDrinker(), bills.getDate(),bills.getItemsPrice(), bills.getTaxPrice(), bills.getTip(), bills.getTotalPrice(), bills.getTime(), bills.getBartender(), bills.getDay())
 		return self.insert(sql,vals)
 
 	def updateBills(self,bills,oldBillId):
-		sql = "UPDATE Bills SET bill_id = %s, bar = %s, drinker = %s, date = %s, items_price = %s, tax_price = %s, tip = %s, total_price = %s, time = %s, bartender = %s, day = %s  WHERE bill_id = %s "
-		vals = (bills.getBillId(), bills.getBar(), bills.getItem(), bills.getQuantity(), bills.getDrinker(), bills.getDate(),bills.getType(), bills.getPrice(), oldBillId)
+		sql = "UPDATE Bills SET bill_id = %s, bar = %s, date = %s, drinker = %s, items_price = %s, tax_price = %s, tip = %s, total_price= %s, time = %s, bartender = %s, day = %s WHERE bill_id = %s "
+		vals = (bills.getBillId(), bills.getBar(), bills.getDate(), bills.getDrinker(), bills.getItemsPrice(), bills.getTaxPrice(), bills.getTip() , bills.getTotalPrice(), bills.getTime(), bills.getBartender(), bills.getDay() , oldBillId)
 		return self.update(sql,vals)
 
 	def deleteBills(self,bills):
